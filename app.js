70483057184a162eb6bedefbcadccd319671c7df
SyedAli310 <alizaidi310@gmail.com>
2021-09-22 18:36:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ $('.toggle-btn').on('click',(e)=>{
 // }
 // })
 
-$("container-main").on('click',(e)=>{
+$(".container-main").on('click',(e)=>{
   if ($('.toggle-btn').find('.tool-tip').length && e.target.id!='toggle-btn' && e.target.id!='toggle-btn-i'){
     $('.tool-tip').css('opacity','0')  
     $('.tool-tip').css('display','none') 
